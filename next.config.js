89c5be4febf4d0e32f9c57d8d2a30dbeebcafd3f
ddocang <bra1024@naver.com>
2025-04-19 20:39:31 +0900

    hj

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export', // Cloudflare Pages에서 사용할 정적 HTML 내보내기 설정
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
